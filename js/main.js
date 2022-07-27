b2fab4086b86f0c275384bd6a828e0b378e58042
riwall <83021906+andrey-riwall@users.noreply.github.com>
2022-07-28 00:34:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,101 @@
+(async function(){
+
+    async function getPosts(page) {
+        const response = await fetch(`https://gorest.co.in/public-api/posts?page=${page}`);
+        const data = await response.json();
+
+        return data;
+    }
+
+    function displayPosts(data) {
+        for (item of data) {
+            const post = createPost(item);
+        }
+
+        const posts = document.querySelectorAll(".posts__item");
+        return posts;
+    }
+
+    function createPost(item) {
+        const posts = document.querySelector(".posts__list");
+
+        const post = document.createElement('li');
+        const link = document.createElement('a');
+        const heading = document.createElement('h2');
+
+        post.classList.add('posts__item');
+        link.classList.add('posts__link');
+        heading.classList.add('posts__title');
+
+        link.href = `./templates/post.html?id=${item.id}`
+        heading.textContent = item.title;
+
+        link.append(heading);
+        post.append(link);
+        posts.append(post);
+
+        return post;
+    }
+
+
+
+    function getPagination(pagination) {
+        let pages = [];
+        for (let i=-1; i<pagination.limit-1; i++) {
+            pages.push(pagination.page+i)
+        }
+        pages = pages.filter(el => el > 0);
+        for (let i=0; i<pagination.limit-pages.length; i++) {
+            pages.push(pages[pages.length-1]+1)
+        }
+        pages = pages.filter(el => el <= pagination.pages)
+
+        return pages;
+    }
+
+    function displayPagination(pages, thisPage) {
+        for (item of pages) {
+            const page = createPagination(item);
+            if (item === thisPage) {
+                page.classList.add('is_active')
+            }
+        }
+
+        const page = document.querySelectorAll(".header__item");
+        return page;
+    }
+
+    function createPagination(item) {
+        const list = document.querySelector(".header__list");
+
+        const page = document.createElement('li');
+        const link = document.createElement('a');
+        
+        page.classList.add('header__item', 'btn');
+        link.classList.add('header__link');
+
+        link.href = `/?page=${item}`
+        link.textContent = item;
+
+        page.append(link);
+        list.append(page);
+
+        return page;
+    }
+
+
+
+    async function engine() {
+        const urlParams = new URLSearchParams(window.location.search)
+        const posts = await getPosts(urlParams.get('page'));
+        const postsEl = displayPosts(posts.data)
+
+        const pagination = getPagination(posts.meta.pagination);
+        const paginationEl = displayPagination(pagination, posts.meta.pagination.page);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        engine();
+    });
+
+})();
---
+++ js/post.js
@@ -0,0 +1,114 @@
+(function(){
+
+    function historyBack() {
+        const btnBack = document.querySelector(".header__link");
+        btnBack.addEventListener('click', (e) => {
+            console.log(history[0]);
+            if (history.length > 2) {
+                history.back();
+            } else {
+                location.replace(location.origin);
+            }
+        })
+    }
+
+
+
+    async function getPost(id) {
+        const response = await fetch(`https://gorest.co.in/public-api/posts/?id=${id}`);
+        const data = await response.json();
+
+        return data;
+    }
+
+    function displayPost(data) {
+        const post = document.querySelector(".post__container");
+
+        const title = document.createElement('h1');
+        const desc = document.createElement('p');
+
+        title.classList.add('post__heading');
+        desc.classList.add('post__desc');
+
+        title.textContent = data.title;
+        desc.textContent = data.body;
+
+        post.append(title, desc);
+    }
+
+
+
+    async function getComments(id) {
+        const response = await fetch(`https://gorest.co.in/public-api/comments?post_id=${id}`);
+        const data = await response.json();
+
+        return data;
+    }
+
+    function displayCommentsList() {
+        const post = document.querySelector(".post__container");
+
+        const comments = document.createElement('div');
+        const heading = document.createElement('h2');
+        const list = document.createElement('ul');
+
+        comments.classList.add('post__comments');
+        heading.classList.add('post__heading_comments');
+        list.classList.add('post__list');
+
+        heading.textContent = 'Комментарии:';
+
+        comments.append(heading, list);
+        post.append(comments);
+    }
+
+    function displayComments(data) {
+        for (item of data) {
+            const comment = createComment(item);
+        }
+
+        const comments = document.querySelectorAll(".post__item");
+        return comments;
+    }
+
+    function createComment(item) {
+        const list = document.querySelector(".post__list");
+
+        const comment = document.createElement('li');
+        const name = document.createElement('h3');
+        const email = document.createElement('span');
+        const body = document.createElement('p');
+
+        comment.classList.add('post__item');
+        name.classList.add('post__name');
+        email.classList.add('post__email');
+        body.classList.add('post__body');
+
+        name.textContent = item.name;
+        email.textContent = item.email;
+        body.textContent = item.body;
+
+        comment.append(name, email, body);
+        list.append(comment);
+    }
+
+
+
+    async function engine() {
+        const urlParams = new URLSearchParams(window.location.search)
+        const post = await getPost(urlParams.get('id'));
+        const postEl = displayPost(post.data[0]);
+        const comments = await getComments(urlParams.get('id'));
+        const commentsListEl = displayCommentsList();
+        const commentsEl = displayComments(comments.data);
+
+        historyBack();
+    }
+
+
+
+    document.addEventListener('DOMContentLoaded', () => {
+        engine();
+    });
+
+})();
